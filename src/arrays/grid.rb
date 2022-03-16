44b30b471723c9659cd2aa9d5194a54afdfe4d8d
Brandon Arreguin <arreguin.tech@gmail.com>
2022-03-15 20:47:40 -0600

    Grid prototype

--- src/arrays/grid.rb
+++ src/arrays/grid.rb
@@ -1,7 +1,27 @@
 require_relative "./array"
 
 module GridModule
-    class Grid < Array
-        #My code here
+    class Grid < ArrayModule::Array
+        def def initialize(rows, columns, default_value="None")
+          @data = Array(rows)
+
+          for row in 0...rows do
+              @data[row] = Array(columns, default_value)
+          end
+        end
+
+        def get_height
+            return @data.length
+        end
+
+        def get_width
+            return @data[0].length
+        end
+
+        def get_item(index)
+            if @data
+                return "Index #{index}: #{@data[index]} item"
+            end
+        end
     end
 end
